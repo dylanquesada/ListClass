ed446fc521e4f4b37108f284fb2cd53f69123d24
Dylan <dquesada186@gmail.com>
2018-01-11 14:44:08 -0600

    Added method to ListClass class for Add.

---
+++ ListClass/ListClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListClass
+{
+    class ListClass<T>
+    {
+        // member variables
+        T[] array;
+        // constructor
+        public ListClass()
+        {
+
+        }
+        //list.Add(1);
+        //list.Remove(1);
+        //list.Zip();
+        //List<int> list = new List<int>();
+
+        // member methods
+        public void Add(ref T input)
+        {            
+            T[] product = new T[array.Length + 1];
+            product[array.Length + 1] = input;
+            for (int i = 0; i < array.Length; i++)
+            {
+                product[i] = array[i];
+            }       
+        }
+    }
+}
